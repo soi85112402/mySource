224fb8148e631550815d1cdcab27b7b4cb401e12
soi85112402 <soi85112402@gmail.com>
2021-08-10 17:28:33 +0900

    포트폴리오 pageCount 체크 코딩중

--- portpolio/animation/wrap_move.js
+++ portpolio/animation/wrap_move.js
@@ -2,6 +2,7 @@
 
 
 const section = document.querySelectorAll('section');
+const contents = document.querySelectorAll('content');
 
 const moveSpeed = 100;
 
@@ -9,15 +10,35 @@ let pageCount = 0;
 let zPoint = 0;
 const tl = new TimelineLite();
 
+// var div_array = Array.prototype.slice.call(div_list)
+const sectionBetween = Array.prototype.slice.call(section).map(item => parseInt(item.style.transform.split(' ').reverse()[0].split(')')[0]));
+
+// console.log(section.filter(item => item.style.transform.translate3d.z));
+// console.log(section.filter((item,index) => index));
+// console.log(section.values());
+
+
+// console.log(section.filter(item => item.style.transform.translate3d.z < 0));
+
+
+
+// console.log(sectionBetween);
+
+
 document.addEventListener('wheel', e=> {
 
     const { wheelDeltaY } = e;
 
-    pageCount = parseInt(zPoint/(1000*(pageCount+1)));
+    
+    // pageCount = parseInt(zPoint/(1000*(pageCount+1 ?? 1)));
+    pageCount = sectionBetween.filter( item => (item - zPoint) > 0);
+    console.log(pageCount);
+    
     // pageCount = (pageCount > 0)? pageCount : 0;
+    
 
     zPoint = ( wheelDeltaY < 0 ) ? zPoint - moveSpeed : zPoint + moveSpeed;
-
+    
     // console.log(section[pageCount].style.transform);
     console.log(zPoint);
     
@@ -29,18 +50,14 @@ document.addEventListener('wheel', e=> {
     }
 
 
-
-
     if ( wheelDeltaY < 0 ) {
         tl.to(section[pageCount], .01, {x:0, y:0, z:'-='+moveSpeed});
+        // tl.to(contents[pageCount], .01, {x:0, y:0, z:'-='+moveSpeed});
     }else{
         tl.to(section[pageCount], .01, {x:0, y:0, z:'+='+moveSpeed});
+        // tl.to(contents[pageCount], .01, {x:0, y:0, z:'+='+moveSpeed});
     }
 
-    
-    
-    
-  
     // section[pageCount].style.transform = `translate3d(0,0,${zPoint + (pageCount * -1000)}px)`;
 
 });
